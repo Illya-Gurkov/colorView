93f5287650964f96c81c595270ebe0820497cfe4
Illya-Gurkov <109282408+Illya-Gurkov@users.noreply.github.com>
2022-10-11 18:15:07 +0300

    new

---
+++ colorView/colorView/colorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorViewApp.swift
+//  colorView
+//
+//  Created by Illya Gurkov on 11.10.22.
+//
+
+import SwiftUI
+
+@main
+struct colorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
